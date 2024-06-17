afc516d45fcc62963b5be24c7621015e968904ca
Thomas Engels <thomas.engels@keysight.com>
2024-06-17 09:07:19 -0400

    Scrolling functionality

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -14,11 +14,49 @@ $(document).ready(function() {
   });
 });
 
+
 document.addEventListener('DOMContentLoaded', function () {
-    var buttons = document.querySelectorAll('.explanations .card-header .btn');
-    buttons.forEach(function(button) {
-        button.addEventListener('mouseenter', function() {
-            button.click();
-        });
+    var first_section_button = document.querySelector("#headingOne button");
+    first_section_button.addEventListener('click', function(){
+        first_a = document.querySelector("#first-collapse")
+        console.log(first_a)
+        first_a.scrollIntoView()
     });
+    var second_section_button = document.querySelector("#headingTwo button");
+    second_section_button.addEventListener('click', function(){
+        second_a = document.getElementById("#second-collapse")
+        second_a.scrollIntoView()
+    });
+    var third_section_button = document.querySelector("#headingThree button");
+    third_section_button.addEventListener('mouseover', function(){
+        third_a = document.getElementById("#third-collapse")
+        third_a.scrollIntoView()
+    });
+
+//    // Smooth scroll to #headingOne on click over button in #headingOne
+//    var button_1 = document.querySelector('#headingOne .btn');
+//    button_1.addEventListener('click', function() {
+//        var targetElement = document.getElementById('headingOne');
+//        if (targetElement) {
+//            targetElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+//        }
+//    });
+//    // Smooth scroll to #headingTwo on click over button in #headingTwo
+//    var button_2 = document.querySelector('#headingTwo .btn');
+//    button_2.addEventListener('click', function() {
+//        var targetElement = document.getElementById('headingTwo');
+//        if (targetElement) {
+//            targetElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+//        }
+//    });
+//
+//    var button_3 = document.querySelector('#headingThree .btn');
+//    button_3.addEventListener('click', function() {
+//        var targetElement = document.getElementById('headingThree');
+//        if (targetElement) {
+//            targetElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+//        }
+//    });
+
+    // Add more event listeners for other buttons as needed
 });
